a62fd124c511de0eeb173d01f6176279fd97f10b
Vivek Yadav <vivekyadav00681@gmail.com>
2025-10-26 19:36:41 +0530

    added an intro page

---
+++ lib/intro_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(236, 224, 209, 1),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/introImage.png'),
+          Text(
+            'Stay Focused',
+            style: TextStyle(
+              color: Color.fromRGBO(56, 34, 15, 1),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            width: 300,
+            child: Text(
+              'Get the cup filled of your choice to stay focused and awake. Diffrent type of coffee menu, hot lottee cappucino',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                color: Color.fromRGBO(68, 68, 68, 0.6),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/menu_item.dart
@@ -0,0 +1,10 @@
+class MenuItem {
+  const MenuItem({
+    required this.name,
+    required this.price,
+    required this.description,
+  });
+  final String name;
+  final int price;
+  final String description;
+}
